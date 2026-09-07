abe8736c18ab5a662813594a42d392fb9ee5abab
soportecolimancff <soportecolimancff@gmail.com>
2026-09-07 11:47:33 -0700

    *Changed texto from Exportar Pagados to Exportar confirmados

--- components/admin/PedidosTable.tsx
+++ components/admin/PedidosTable.tsx
@@ -479,12 +479,12 @@ export default function PedidosTable({ pedidos, sucursales }: Props) {
           <button
             onClick={exportarExcel}
             className="flex items-center gap-1.5 rounded-xl border border-green-300 bg-green-50 px-3 py-1.5 text-xs font-semibold text-green-700 hover:bg-green-100 active:scale-95 transition-all"
-            title="Exportar pedidos PAGADOS a Excel"
+            title="Exportar pedidos CONFIRMADOS a Excel"
           >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
             </svg>
-            Exportar pagados
+            Exportar confirmados
           </button>
         </div>
       </div>
